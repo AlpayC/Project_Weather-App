69df6972d333cfc6c80e47396fe4dc26d7cd5421
AlpayC <alpaycelik91@googlemail.com>
2023-05-29 22:04:40 +0200

    upload project with complete styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,28 @@
 const apiKey = "644ce177f2832521a036c130adc9e8da";
 
 const getWeatherData = () => {
+  // Vorherige Inhalte werden gelöscht
+  const outputOne = document.querySelector(".outputcontainer");
+  outputOne.innerHTML = "";
+  const outputTwo = document.querySelector(".forecastcontainer");
+  outputTwo.innerHTML = "";
+  const outputThree = document.querySelector(".outputcontainersec");
+  outputThree.innerHTML = "";
+
   // Der Input imt Textfeld wird nun in der Variable gespeichert
   let city = document.querySelector("#city").value;
-  let cityBackground = document.querySelector(".cityimg");
-  fetch(
-    `https://pixabay.com/api/?key=36815996-aa926cadda8d303d9cfe5178f&q=${city}+city`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log(data.hits[0].largeImageURL);
-      cityBackground.style.backgroundImage = `url(${data.hits[0].largeImageURL})`;
-    })
-    .catch((error) => console.log("Fehler beim Laden: ", error));
 
-  // Es wird geprüft, ob im Input eine Zahl oder ein Text enthalten ist. typeof city gibt selbst bei einer Zahl immer String aus. Daher habe ich !isNan als Prüfoperator genutzt. So ist eine if else bedingung ob Zahl oder String möglich
+  // Es wird geprüft, ob im Input eine Zahl oder ein Text enthalten ist. typeof city gibt selbst bei einer Zahl immer String aus. Daher habe ich !isNan als Prüfoperator genutzt. So ist eine if else bedingung ob Zahl oder String möglich. Wird eine Stadt gefunden, die die API für uns finden kann, dann werden die geladenen Daten an die Funktion processData übergeben
   if (!isNaN(city)) {
-    console.log("Variable is a number");
-
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=de,&appid=${apiKey}&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         processData(data);
       })
       .catch((error) => console.log("Fehler beim Laden: ", error));
   } else if (typeof city === "string") {
-    console.log("Variable is a string");
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city},&appid=${apiKey}&lang=de&units=metric`
     )
@@ -40,17 +33,18 @@ const getWeatherData = () => {
       .catch((error) => console.log("Fehler beim Laden: ", error));
   } else {
     window.alert("Bitte gebe eine PLZ oder die Stadt ein!");
-    console.log("keine eingabe");
   }
 };
 
 const processData = (data) => {
-  console.log(data);
+  // Alle Daten aus dem Datenpaket des obigen Fetches werden in passende Variablen gespeichert.
+  // Die Eingabe der Stadt wird nochmals in einer Variable gespeichert, damit im Städtehintergrund der Name der Stadt später im Output ausgegeben wird
+  let inputValue = city.value;
   const cityName = data.name;
   const coordLat = data.coord.lat;
   const coordLon = data.coord.lon;
+  // Die Breitengrade werden ebenfalls gespeichert, damit die Wetterkarte exakte Breitengrade erhält und das Iframe vernünftig funktionieren kann
   const coordinatesWeatherCard = `?lat=${data.coord.lat}&lon=${data.coord.lon}&detailLat=${data.coord.lat}&detailLon=${data.coord.lon}`;
-  console.log(coordinatesWeatherCard);
   const humidity = data.main.humidity;
   const pressure = data.main.pressure;
   const feelsLike = data.main.feels_like;
@@ -63,9 +57,11 @@ const processData = (data) => {
   let timeZone = data.timezone;
   const visbility = data.visibility;
   const weatherDescription = data.weather[0].description;
+  // Das Wettericon wird trotz eigener Bilder geladen. Die Iconbezeichnungen sind vorher an die Dateinamen meiner persönlichen Icons angepasst worden, damit ich lediglich beim Output meinen lokalen Pfad nutzen kann
   const weatherIcon = data.weather[0].icon;
   let windDegree = data.wind.deg;
   let windSpeed = data.wind.speed;
+  // Die Degreezahl im Datenfetch wird ebenfalls in einer seperaten Variable gespeichert, damit beim Output der Pfeil in die richtige Richtung gedreht werden kann.
   let windArrowDirection = data.wind.deg;
   //  Berechnung der Windrichtung. Es wird die Degree Zahl geprüft und entsprechend mit einer Himmelsrichtung verheiratet und später ausgegeben.
   let i = windDegree;
@@ -110,17 +106,15 @@ const processData = (data) => {
   sunRise = new Date(sunRise * 1000);
   sunSet = new Date(sunSet * 1000);
 
-  // Berechnung der Windgeschindigkeit von m/s in km/h
+  // Berechnung der Windgeschindigkeit von m/s in km/h. Dabei habe ich allgemein die Zahl mit 3.6 multipliziert
   windSpeed = (data.wind.speed * 3.6).toFixed(2);
-  // Berechnung der Zeiten
-  let localeTime = new Date().getTime();
-  console.log(localeTime);
-  let date = new Date(localeTime - 3600 * 2000 + data.timezone * 1000);
-  console.log(date);
-  timeZone = date.toLocaleTimeString();
-  console.log(timeZone);
 
-  // Fetchen der aktuellen Luftverschmutzung
+  // Berechnung der Zeiten in eine kurze Schreibweise
+  let localeTime = new Date().getTime();
+  let date = new Date(localeTime - 3600 * 2000 + data.timezone * 1000);
+  timeZone = date.toLocaleTimeString();
+
+  // Fetchen der aktuellen Luftverschmutzung durch die openweather API
   let airPollution;
   let airPollutionRating;
   fetch(
@@ -129,6 +123,7 @@ const processData = (data) => {
     .then((res) => res.json())
     .then((data) => {
       airPollution = data.list[0].main.aqi;
+      console.log(airPollution);
       if (airPollution == 1) {
         airPollutionRating = "Gut";
       } else if (airPollution == 2) {
@@ -142,15 +137,13 @@ const processData = (data) => {
       }
     })
     .catch((error) => console.log("Fehler beim Laden: ", error));
-  // Fetchen des 5 Tage ForeCasts bzw der 3 Stunden Forecasts
+  // Fetchen des 5 Tage ForeCasts bzw der 3 Stunden Forecasts durch die openweather API. Dabei werden alle möglichen Timestamps in seperate HTML Objekte gespeichert.
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${coordLat}&lon=${coordLon}&appid=${apiKey}&units=metric&lang=de`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       let outputForecast = document.querySelector(".forecastcontainer");
-
       const timeInterval = data.list;
       timeInterval.forEach((timestamp) => {
         const today = new Date();
@@ -174,8 +167,6 @@ const processData = (data) => {
           monthApi == todayMonth &&
           yearApi == todayYear
         ) {
-          console.log("heute");
-          console.log(hoursApi);
           foreCastTitleTxt = `Heute`;
           foreCastTitleTime = `${hoursApi}:00 Uhr`;
           foreCastTitleDate = `${fullDateApi}`;
@@ -187,7 +178,6 @@ const processData = (data) => {
             monthApi - todayMonth == 1 &&
             yearApi == todayYear)
         ) {
-          console.log("nein");
           foreCastTitleTxt = `Morgen`;
           foreCastTitleTime = `${hoursApi}:00 Uhr`;
           foreCastTitleDate = `${fullDateApi}`;
@@ -221,7 +211,7 @@ const processData = (data) => {
         </div>`;
         outputForecast.insertAdjacentHTML("beforeend", foreCastContainer);
       });
-      // Output des aktuellen Wetters MAIN
+      // Output des aktuellen Wetters MAIN Linker Container
       // <img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png" alt="" class="weathericon" />
       let outputMainData = document.querySelector(".outputcontainer");
       const outputContainerMain = `<h1 class="weathertitle">Wetter in ${cityName},${country}</h1>
@@ -231,17 +221,18 @@ const processData = (data) => {
   </div>
   <p class="cloudinfooutput">${weatherDescription}</p>
   <p class="actualtimeoutput">Daten erhalten am ${new Date().toLocaleDateString()} um ${new Date().toLocaleTimeString()} Uhr</p>
-  <div class=cityimg></div>
+  <div class=cityimg><h2 class=citynameimg> </h2></div>
   <div class="localtimecontainer">
     <h3>Local time</h3>
     <h3 class="localtimeoutput">${timeZone}</h3>
   </div>
   `;
-
       outputMainData.insertAdjacentHTML("afterbegin", outputContainerMain);
+      // Output des aktuellen Wetters MAIN Rechter Container mit Zusatzdaten und der Wetterkarte
+
       let outputSecData = document.querySelector(".outputcontainersec");
-      const outputContainerSecondary = `<h2 class=todayhighlights>Today's Highlights</h2>
-      <section class=secondaryitems>
+      const outputContainerSecondary = `<h2 class=todayhighlights id=innersecitems>Today's Highlights</h2>
+      <section class=secondaryitems id=innersecitems>
       <div class="windcontainer">
       <h3>Wind</h3>
       <article class=windinfodescrcontainer><img class="windarrow" src="../assets/img/uparrow.png" > <h4 class="windoutput">${windSpeed} <span class=innerunits> km/h</span></h4></article>
@@ -281,7 +272,8 @@ const processData = (data) => {
         </div>
  </section>
 `;
-      const weatherCardOutput = `<div class="coordcontainer">
+      //Output der Wetterkarte in ein Iframe, wo zuvor die Breitengrade in einer Variable gespeichert wurden
+      const weatherCardOutput = `<div class="coordcontainer" id=innersecitems>
       <h3 class=weathercardtitle>Wetterkarte</h3>
       <iframe class=iframeweathercard width="100%" height="600px" src="https://embed.windy.com/embed2.html${coordinatesWeatherCard}&width=1000&height=1000&zoom=10&level=surface&overlay=rain&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1" frameborder="0"></iframe>
       <h3 class="coordoutput">Koordinaten[${coordLat.toFixed(
@@ -296,18 +288,35 @@ const processData = (data) => {
       document.querySelector(
         ".windarrow"
       ).style.transform = `rotate(${windArrowDirection}deg)`;
+
+      // Output des Hintergrundbildes der eingegebnen Stadt
+      let cityBackground = document.querySelector(".cityimg");
+      let cityBackgroundUrl;
+      fetch(
+        `https://pixabay.com/api/?key=36815996-aa926cadda8d303d9cfe5178f&q=${inputValue}+city`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          let cityNameImg = document.querySelector(".citynameimg");
+          cityNameImg.innerHTML = inputValue;
+          cityBackgroundUrl = data.hits[0].largeImageURL;
+          cityBackground.style.backgroundImage = `url("${data.hits[0].largeImageURL}")`;
+        })
+        .catch((error) => console.log("Fehler beim Laden: ", error));
+
+      cityBackground.style.backgroundImage = `url("${cityBackgroundUrl}")`;
     })
     .catch((error) => console.log("Fehler beim Laden: ", error));
 };
 
-// Ausführung der App beim Betätigen der Enter Taste. Es wird beim Entern auf dem "Submit" B utton geklickt und die Funktion ausgeführt
+// Ausführung der App beim Betätigen der Enter Taste. Es wird beim Entern auf dem "Submit" Button geklickt und die Funktion ausgeführt
 city.addEventListener("keypress", (e) => {
   if (e.key == "Enter") {
     getWeatherData();
   }
 });
 
-// Macht die Forecastsektion swipeable
+// Macht die Forecastsektion swipeable. Ein Scrollbalken ist hier nicht zu sehen. Der Bereich ist sowohl mit der Maus als auch per Mausklick und Touch swipeable
 
 const container = document.querySelector(".forecastcontainer");
 const innerContainers = document.querySelectorAll(".forecastitem");
@@ -366,5 +375,5 @@ container.addEventListener("wheel", (event) => {
   event.preventDefault();
   container.scrollLeft += event.deltaY;
 });
-// Set start position to the leftmost side of the container
+// Der Forecastcontainer startet immer an erster Stelle, um responsive Probleme zu umgehen.
 const firstInnerContainer = innerContainers[0];
